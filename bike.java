78968c9311f7640c67b7df81100a8ed6f11b497e
khanmca <38030403+khanmca@users.noreply.github.com>
2018-04-03 12:56:11 +0530

    Add files via upload

---
+++ bike.java
@@ -0,0 +1,11 @@
+
+public class bike {
+	void run()
+	{
+		System.out.println("run safely");
+	}
+}
+
+	
+
+
---
+++ splender.java
@@ -0,0 +1,13 @@
+
+public class splender extends bike{
+
+	void run(){
+		//super.run();
+		System.out.println("running 60 km");
+	}
+	
+public static void main(String[] args){
+bike s1=new splender();
+s1.run();
+}
+}
